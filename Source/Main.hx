e21a12028d335b9d67f446dbcc4e44f1bd5c03eb
saumya <2saumyaray@gmail.com>
2017-04-27 17:03:28 +0530

    init

---
+++ Source/Main.hx
@@ -0,0 +1,24 @@
+package;
+
+
+import openfl.display.BitmapData;
+import openfl.display.Bitmap;
+import openfl.display.Sprite;
+import openfl.Assets;
+
+
+class Main extends Sprite {
+	
+	
+	public function new () {
+		
+		super ();
+		
+		var bitmapData = Assets.getBitmapData ("assets/91825.png");
+		var bitmap = new Bitmap (bitmapData);
+		addChild (bitmap);
+		
+	}
+	
+	
+}
